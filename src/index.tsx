23ee433c6559f1a2784622b8eef1a7c303c2b7e6
Mihail <mihalisp@protonmail.com>
2022-01-20 18:43:08 +0200

    added mui

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,33 @@
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './Components/App';
-import reportWebVitals from './Rest/reportWebVitals';
+//import reportWebVitals from './Rest/reportWebVitals';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#BB86FC'
+        },
+        secondary: {
+            main: '#03DAC6'
+        }
+    },
+})
+
+ReactDOM.render(
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+    </ThemeProvider>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
